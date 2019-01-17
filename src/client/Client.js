f1652dbe5f27a802fe4c7219f552eac675cb1779
Owen Bellish <shadowguy224@live.com>
2019-01-17 10:50:24 -0500

    fixed authentication error logging

--- src/client/Client.js
+++ src/client/Client.js
@@ -11,8 +11,8 @@ class SequelizeClient extends EventEmitter {
     constructor(database) {
         super();
         this.database = database;
-        database.authenticate().catch(error => console.error('[AUTHENTICATING] The client had an error authenticating with the database'));
-        database.sync().catch(error => console.error('[SYNCING] The client has an error syncing the database', error));
+        database.authenticate().catch(error => console.error('[AUTHENTICATING] The client had an error authenticating with the database\n', error));
+        database.sync().catch(error => console.error('[SYNCING] The client has an error syncing the database\n', error));
         this.events = new EventManager(this, database);
     }
 }
