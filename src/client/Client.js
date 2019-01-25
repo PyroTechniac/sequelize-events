b54f52b5e9991d9df336e0102c19182ae7df9b7a
Owen Bellish <shadowguy224@live.com>
2019-01-25 10:33:02 -0500

    adding sync and validating options

--- src/client/Client.js
+++ src/client/Client.js
@@ -6,13 +6,33 @@ const EventManager = require('../events/EventManager');
  */
 class SequelizeClient extends EventEmitter {
     /**
+     * @constructor
      * @param {SequelizeDatabase} database - Database for the client
-     * @param {Object} options - Options for the client
      */
     constructor(database) {
         super();
         this.database = database;
         this.events = new EventManager(this, database);
+    }
+    /**
+     * @param {Object} [options] - options for syncing
+     * @returns {Promise<SequelizeDatabase>}
+     */
+    async sync(options) {
+        await this._validateSyncOptions(options);
+    }
+    /**
+     * @param {object}
+     * @private
+     */
+    _validateSyncOptions(options) {
+        console.log(typeof options);
+        if (typeof options !== 'undefined' && typeof options !== 'object') {
+            throw new TypeError('sync options must be an object or undefined');
+        }
+        const isArr = options instanceof Array;
+        if (isArr === true) throw new TypeError('sync options must be an object or undefined');
+        if (typeof options === 'undefined') return;
     }
 }
 module.exports = SequelizeClient;
--- test/test.js
+++ test/test.js
@@ -9,18 +9,11 @@ const db = new Sequelize('database', 'username', 'password', {
     host: 'localhost'
 });
 const client = new Client(db);
+client.sync(['test']);
 client.database.sync({ force: true });
 const test = db.define('test', {
     ranNum: Sequelize.INTEGER
 });
 test.create({
     ranNum: Math.floor(Math.random() * 32)
-});
-client.on('afterCreate', (test1, test2) => {
-    console.log(test1);
-    console.log(test2);
-});
-client.on('beforeCreate', (test3, test4) => {
-    console.log(test3);
-    console.log(test4);
 });
