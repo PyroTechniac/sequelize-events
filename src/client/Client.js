c66a5cac80f595828c2357c99e1e6e6d165ad898
Owen Bellish <shadowguy224@live.com>
2019-01-25 10:56:17 -0500

    JSDoc writing stuff

--- src/client/Client.js
+++ src/client/Client.js
@@ -11,6 +11,10 @@ class SequelizeClient extends EventEmitter {
      */
     constructor(database) {
         super();
+        /**
+         * @type {SequelizeDatabase}
+         * @type {EventManager}
+         */
         this.database = database;
         this.events = new EventManager(this, database);
     }
